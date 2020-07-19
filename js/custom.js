ba8ab4fe0ff34af1093b04d8fe03f0c38284519a
Kartik Prajapati <57929830+kpcodedesigner@users.noreply.github.com>
2020-07-19 11:41:58 +0530

    Code uplode

---
+++ js/custom.js
@@ -0,0 +1,77 @@
+﻿
+$().ready(function () {
+
+    /************************ Project slider ********************************/
+
+    $('.our_project_box').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    });
+
+    /************************ Team slider ********************************/
+
+    $('.our_team_box').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    });
+  
+    /************************ Testimonial Slider ********************************/
+
+    $('.testimonial_desk').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    });
+
+   /************************ Header sticky ********************************/
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.header_menu').addClass('newClass');
+        } else {
+            $('.header_menu').removeClass('newClass');
+        }
+    });
+
+    
+
+});
+
